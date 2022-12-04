a60ea6a160f807692a5c965cb2a19db126213d13
mason369 <maosenyang00@gmail.com>
2022-12-04 16:18:33 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,12 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({transpileDependencies: true})
+module.exports = {
+    css: {
+      loaderOptions: {
+        scss: {
+            additionalData: `@import "@/assets/scss/global";`
+        }
+      }
+      
+    }
+  }
