c91cafdb203d34853926ad39f61aa9b5733e88ef
mason369 <maosenyang00@gmail.com>
2022-12-05 16:49:33 +0800

    fix bug:rename

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-const { defineConfig } = require("@vue/cli-service")
+const {defineConfig} = require("@vue/cli-service")
 module.exports = defineConfig({transpileDependencies: true})
-module.exports = {css: {loaderOptions: {scss: {additionalData: "@import \"@/assets/scss/global\";"}}}}
+// module.exports = {css: {loaderOptions: {scss: {additionalData: "@import \"@/assets/scss/global\";"}}}}
