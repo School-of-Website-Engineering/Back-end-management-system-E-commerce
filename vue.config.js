2b086cb316a7bce7724b3b35eeb37decf4cf6819
mason369 <maosenyang00@gmail.com>
2022-12-05 17:09:45 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 const {defineConfig} = require("@vue/cli-service")
 module.exports = defineConfig({transpileDependencies: true})
 // module.exports = {css: {loaderOptions: {scss: {additionalData: "@import \"@/assets/scss/global\";"}}}}
+module.exports = {
+	lintOnSave: false,
+	publicPath: "/Back-end-management-system-E-commerce/",
+	devServer : {
+		allowedHosts: [
+			"vps3.pista.top", // 允许访问的域名地址，即花生壳内网穿透的地址
+			".pista.top"   // .是二级域名的通配符
+		]
+	}
+};
