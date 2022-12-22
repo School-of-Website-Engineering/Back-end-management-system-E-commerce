c4c17adc08e74c3f37aa69c8b3154c789382dad2
mason369 <1960638223@qq.com>
2022-12-22 13:26:43 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from "vue"
-import App from "./App.vue"
-import router from "./router"
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
 //使用element ui插件
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({router, render: h => h(App)}).$mount("#app")
+new Vue({ router, render: (h) => h(App) }).$mount("#app");
