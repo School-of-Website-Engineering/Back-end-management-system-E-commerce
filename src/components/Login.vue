1c6559b9bcb6b4d2794bfa2d109259135360f886
mason369 <1960638223@qq.com>
2022-12-25 15:04:51 +0800

    update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,16 @@
 <template>
-	<div>
-	sdafd
+	<div class="login-container">
+		登录组件
 	</div>
 </template>
 
 <script>
-export default {name: "Login"}
+export default { name: "Login" };
 </script>
 
 <style lang="scss" scoped>
-
+//登录组件背景颜色
+.login-container {
+	background-color: #2b4b6b;
+}
 </style>
