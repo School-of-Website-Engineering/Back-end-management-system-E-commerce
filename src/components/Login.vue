404a117bce72b9ee4b44dad528d6862e2ace5b99
mason369 <1960638223@qq.com>
2022-12-25 15:17:52 +0800

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="login-container">
-		登录组件
+		<div class="login-box">
+			<div class="avatar-box">
+				<img src="../assets/logo.png" alt="" />
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -12,5 +16,35 @@ export default { name: "Login" };
 //登录组件背景颜色
 .login-container {
 	background-color: #2b4b6b;
+	height: 100vh;
+}
+//登录盒子居中
+.login-box {
+	width: 500px;
+	height: 500px;
+	background-color: #fff;
+	border-radius: 5px;
+	//定位居中
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+	.avatar-box {
+		height: 130px;
+		width: 130px;
+		padding: 12px;
+		border-radius: 50%;
+		border: 1px solid #ccc;
+		background-color: #fff;
+		position: absolute;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		img {
+			height: 100%;
+			width: 100%;
+			border-radius: 50%;
+			background-color: #eee;
+		}
+	}
 }
 </style>
