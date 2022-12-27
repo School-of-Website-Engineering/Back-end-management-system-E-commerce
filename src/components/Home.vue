1a2c2163f3ef1207b64ef660753cc1a96d99225c
mason369 <1960638223@qq.com>
2022-12-27 14:56:49 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,13 +2,39 @@
 	<el-container class="home-container">
 		<!-- 头部 -->
 		<el-header>
+			<div>
+				<img src="../assets/logo.png" alt="" />
+				<span>电商后台管理系统</span>
+			</div>
 			<!--退出按钮-->
 			<el-button type="danger" @click="logout">退出</el-button></el-header
 		>
 		<!-- 页面主体 -->
 		<el-container>
 			<!-- 侧边栏 -->
-			<el-aside width="200px"></el-aside>
+			<el-aside width="200px">
+				<el-menu
+					background-color="#333744"
+					text-color="#fff"
+					active-text-color="#ffd04b"
+				>
+					<!-- 一级菜单 -->
+					<el-submenu index="1">
+						<!-- 模板区域 -->
+						<template slot="title">
+							<i class="el-icon-location"></i>
+							<span>导航一</span>
+						</template>
+						<!-- 二级菜单 -->
+						<el-menu-item index="1-1">
+							<template slot="title">
+								<i class="el-icon-location"></i>
+								<span>导航一</span>
+							</template>
+						</el-menu-item>
+					</el-submenu>
+				</el-menu>
+			</el-aside>
 			<!-- 右侧内容主体区域 -->
 			<el-main></el-main>
 		</el-container>
@@ -30,15 +56,31 @@ export default {
 
 <style lang="scss" scoped>
 .el-header {
-	background-color: #373D41;
+	background-color: #373d41;
+	display: flex;
+	justify-content: space-between;
+	padding-left: 0;
+	align-items: center;
+	color: #fff;
+	img {
+		height: 28px;
+	}
+	div {
+		display: flex;
+		align-items: center;
+		span {
+			margin-left: 10px;
+			font-size: 18px;
+		}
+	}
 }
-.el-aside{
-	background-color: #333744
+.el-aside {
+	background-color: #333744;
 }
-.el-main{
+.el-main {
 	background-color: #eaedf1;
 }
-.home-container{
+.home-container {
 	height: 100vh;
 }
 </style>
