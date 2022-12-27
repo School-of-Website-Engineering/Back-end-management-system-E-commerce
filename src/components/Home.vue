7a43733e0c57d1b7360f547004dac15b6f30afc2
mason369 <1960638223@qq.com>
2022-12-27 13:56:42 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,21 @@
 <template>
 	<div>
-	
+		<!--退出按钮-->
+		<el-button type="danger" @click="logout">退出</el-button>
 	</div>
 </template>
 
 <script>
-export default {name: "Home"}
+export default {
+	name   : "Home",
+	methods: {
+		//	退出登录
+		logout(){
+			window.sessionStorage.clear()
+			this.$router.push("/login")
+		}
+	}
+}
 </script>
 
 <style lang="scss" scoped>
