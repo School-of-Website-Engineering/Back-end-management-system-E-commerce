efbf62f6d802b398839ff1d8981747f74a414716
mason369 <1960638223@qq.com>
2023-01-04 15:24:04 +0800

    Update Cate.vue
    Co-Authored-By: liuliu66 <101164913+liusxs@users.noreply.github.com>

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -1,5 +1,26 @@
 <template>
-	<div></div>
+	<div>
+		<!--面包屑导航-->
+		<el-breadcrumb separator-class="el-icon-arrow-right">
+			<el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+			<el-breadcrumb-item>商品管理</el-breadcrumb-item>
+			<el-breadcrumb-item>商品分类</el-breadcrumb-item>
+		</el-breadcrumb>
+		<br>
+		<!--卡片视图区-->
+		<el-card>
+			<el-row>
+				<el-col>
+					<el-button type="primary">
+						添加分类
+					</el-button>
+				</el-col>
+			</el-row>
+			<!-- 表格 -->
+			
+			<!-- 分页区 -->
+		</el-card>
+	</div>
 </template>
 
 <script>
