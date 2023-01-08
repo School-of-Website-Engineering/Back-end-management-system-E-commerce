ac305bf775e759fcbb2450d8a5fff5476484fa92
mason369 <1960638223@qq.com>
2023-01-08 21:26:30 +0800

    Update Order.vue

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -16,12 +16,79 @@
 					</el-input>
 				</el-col>
 			</el-row>
+			<!-- 订单列表数据 -->
+			<br />
+			<el-table :data="orderlist" border stripe>
+				<el-table-column type="index" label="#"></el-table-column>
+				<el-table-column label="订单编号" prop="order_number"></el-table-column>
+				<el-table-column label="订单价格" prop="order_price"></el-table-column>
+				<el-table-column label="是否付款" prop="pay_status">
+					<template v-slot="scope">
+						<el-tag v-if="scope.row.pay_status == 1" type="success"
+							>已付款</el-tag
+						>
+						<el-tag v-else type="danger">未付款</el-tag>
+					</template>
+				</el-table-column>
+				<el-table-column label="是否发货" prop="is_send"></el-table-column>
+				<el-table-column label="下单时间" prop="create_time">
+					<template v-slot="scope">
+						{{ scope.row.create_time | dateFormat }}
+					</template>
+				</el-table-column>
+				<el-table-column label="操作">
+					<template v-solt="scope">
+						<!-- 带图标的button -->
+						<el-button
+							type="primary"
+							icon="el-icon-edit"
+							size="mini"
+						></el-button>
+						<el-button
+							type="success"
+							icon="el-icon-location"
+							size="mini"
+						></el-button>
+					</template>
+				</el-table-column>
+			</el-table>
 		</el-card>
 	</div>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			queryInfo: {
+				query   : "",
+				pagenum : 1,
+				pagesize: 10
+			},
+			total    : 0,
+			orderlist: []
+		};
+	},
+	methods: {
+		async getOrderList() {
+			const { data: res } = await this.$http.get("orders", {params: this.queryInfo});
+			//获取数据失败
+			if (res.meta.status !== 200) {
+				return this.$message.error("获取订单列表失败");
+			}
+			//获取数据成功
+			this.orderlist = res.data.goods;
+			this.total = res.data.total;
+		}
+	},
+	mounted() {
+		this.getOrderList();
+	}
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+::v-deep .el-table .el-table__cell {
+	text-align: center;
+}
+</style>
