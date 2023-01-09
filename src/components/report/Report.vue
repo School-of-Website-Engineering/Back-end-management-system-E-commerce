c48c58e5895f86d67de6404595b4338b970e3943
mason369 <1960638223@qq.com>
2023-01-09 13:07:19 +0800

    创建"report"组件
    Co-Authored-By: liuliu66 <101164913+liusxs@users.noreply.github.com>

---
+++ src/components/report/Report.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<!-- 面包屑导航 -->
+		<el-breadcrumb separator-class="el-icon-arrow-right">
+			<el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+			<el-breadcrumb-item>订单管理</el-breadcrumb-item>
+			<el-breadcrumb-item>订单列表</el-breadcrumb-item>
+		</el-breadcrumb>
+		<!-- 卡片视图 -->
+		<br />
+		<el-card> </el-card>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Report",
+	data() {
+		return {};
+	},
+	methods: {},
+	mounted() {}
+};
+</script>
+
+<style scoped lang="scss"></style>
